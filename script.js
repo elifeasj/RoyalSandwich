320975f22a88ba13cf6b46417a4eabf3f0c964ed
effy <elif0757@edu.easj.dk>
2022-05-28 20:59:57 +0200

    linket til javascript filen script.js på alle sider

---
+++ script.js
@@ -0,0 +1,7 @@
+function bounceText ()
+let element = document.querySelector('section')
+
+element.classList.toggle('animate__fadeInDown-')
+
+
+/* Javascript animationerne er inspireret af siden https://animate.style/ */
