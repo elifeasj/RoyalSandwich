94777a8238b69db1d7ebdcac561d3ffd61fb7cce
effy <elif0757@edu.easj.dk>
2022-05-29 22:19:29 +0200

    Tilføjelser af javascript: fadeIn

--- script.js
+++ script.js
@@ -3,5 +3,9 @@ let element = document.querySelector('section')
 
 element.classList.toggle('animate__fadeInDown-')
 
+function bounceText ()
+let element = document.querySelector('section')
+
+element.classList.toggle('animate__fadeIn-')
 
 /* Javascript animationerne er inspireret af siden https://animate.style/ */
