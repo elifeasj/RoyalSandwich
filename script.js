b0a8ff48d620149cf196b137f891adaef3a6a0df
effy <elif0757@edu.easj.dk>
2022-05-30 23:57:04 +0200

    Tilføjelse af Javascript

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+/* Javascript animationerne er inspireret af siden https://animate.style/ */
+
 function fadeInText () {
 let element = document.querySelector('section')
 
 element.classList.toggle('animate__fadeIn')
 }
 
-/* Javascript animationerne er inspireret af siden https://animate.style/ */
+function myFunction() {
+    alert("Tak for din besked! Vi vender tilbage hurtigst muligt! :)");
+  }
