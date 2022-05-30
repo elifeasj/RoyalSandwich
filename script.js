9dec978a995d03e85bb69981d13eb16425071905
effy <elif0757@edu.easj.dk>
2022-05-30 21:53:52 +0200

    Dropdrown menu - done og tilføjet på alle sider

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-function bounceText ()
+function fadeInText () {
 let element = document.querySelector('section')
 
-element.classList.toggle('animate__fadeInDown-')
-
-function bounceText ()
-let element = document.querySelector('section')
-
-element.classList.toggle('animate__fadeIn-')
+element.classList.toggle('animate__fadeIn')
+}
 
 /* Javascript animationerne er inspireret af siden https://animate.style/ */
