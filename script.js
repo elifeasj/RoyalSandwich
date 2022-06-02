0a451b76665ffac4760032a02912b6ff622d0f80
effy <elif0757@edu.easj.dk>
2022-06-02 15:54:03 +0200

    rettelser for seo

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-/* Javascript animationerne er inspireret af siden https://animate.style/ */
-
-function fadeInText () {
-let element = document.querySelector('section')
-
-element.classList.toggle('animate__fadeIn')
-}
-
 function myFunction() {
     alert("Tak for din besked! Vi vender tilbage hurtigst muligt! :)");
   }
